9e60d525052008f719b98b120d08201551b46164
thisisgaganbirru <gagansai9090@gmail.com>
2021-06-05 10:05:33 -0500

    chore: upgraded terraform to 0.14 and added required_providers

--- backend.tf
+++ backend.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "terraform-aws/terraform.tfstate"
