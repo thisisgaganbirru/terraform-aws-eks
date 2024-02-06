43427def085bf685916ea2af61008cbe2d0ccae8
thisisgaganbirru <gagansai9090@gmail.com>
2024-02-05 18:14:23 -0600

    chore: upgrade terraform to 1.6 and aws provider to 5.0, add tls provider

--- backend.tf
+++ backend.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
 
