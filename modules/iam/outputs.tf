416870ece7c9ce1406cd91212a0fa99133e9ce7d
thisisgaganbirru <gagansai9090@gmail.com>
2024-02-15 18:33:52 -0600

    feat: add ebs csi driver and aws load balancer controller iam roles

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -22,3 +22,13 @@ output "cluster_autoscaler_role_arn" {
   description = "IAM role ARN for cluster autoscaler"
   value       = aws_iam_role.cluster_autoscaler.arn
 }
+
+output "ebs_csi_driver_arn" {
+  description = "IAM role ARN for EBS CSI driver"
+  value       = aws_iam_role.ebs_csi_driver.arn
+}
+
+output "aws_load_balancer_controller_arn" {
+  description = "IAM role ARN for AWS Load Balancer Controller"
+  value       = aws_iam_role.aws_load_balancer_controller.arn
+}
