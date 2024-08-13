b749d89ba12cdb36975dd0a1d8b17a124401fae8
thisisgaganbirru <gagansai9090@gmail.com>
2024-08-12 20:03:51 -0500

    feat: upgrade eks cluster to 1.29 and bump managed addon versions

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.29"
 }
 
 variable "vpc_id" {
